201124e791a58684f4b4ec4cf63e1f1dd8305495
alexnsu <alexnssm@gmail.com>
2014-05-06 13:49:18 +0200

    A exitwindow

---
+++ src/inda13projekt/ExitWindow.java
@@ -0,0 +1,42 @@
+package inda13projekt;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * 
+ * @author Alex
+ *
+ */
+public class ExitWindow implements Window {
+
+	/**
+	 * 
+	 */
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void render(GameContainer gc, Graphics g, Camera camera)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public Window getNextWindow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
